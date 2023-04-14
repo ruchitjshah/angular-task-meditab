4ee86f5c36d95238364f51acee64f75e1524a30f
Ruchit Shah <ruchitjshah@gmail.com>
2023-04-14 19:11:41 +0530

    commit from ofc 14-04-2023

--- quizeApp/src/app/quiz/quiz.component.ts
+++ quizeApp/src/app/quiz/quiz.component.ts
@@ -5,103 +5,95 @@ import { QuestionService } from '../service/question.service';
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.css']
+  styleUrls: ['./quiz.component.css'],
 })
-export class QuizComponent implements OnInit{
-
+export class QuizComponent implements OnInit {
   public timer: number = 10;
-  public userName: string = "";
+  public userName: string = '';
   public questionList: any = [];
   public currentQuestion: number = 0;
   public counter: number = this.timer;
   public points: number = 0;
   public correctAnswer: number = 0;
   public incorrectAnswer: number = 0;
-  public interval$: any; 
+  public interval$: any;
 
   isQuizCompleted: boolean = false;
 
-  constructor(private questionService: QuestionService){
-
-  }
-  ngOnInit(){
-    this.userName = localStorage.getItem("name")!;
+  constructor(private questionService: QuestionService) {}
+  ngOnInit() {
+    this.userName = localStorage.getItem('name')!;
     this.getAllQuestions();
     this.startCounter();
   }
 
-  getAllQuestions(){
-    this.questionService.getQuestionsJson().subscribe(res =>{
-        this.questionList = res.questions;
-    })
+  getAllQuestions() {
+    this.questionService.getQuestionsJson().subscribe((res) => {
+      this.questionList = res.questions;
+    });
   }
 
-  nextQuestion(){
+  nextQuestion() {
     this.currentQuestion++;
-    
   }
-  previousQuestion(){
+  previousQuestion() {
     this.currentQuestion--;
   }
 
-  answer(currentQno: number, option: any){
-
-    if(currentQno === this.questionList.length ){
+  answer(currentQno: number, option: any) {
+    if (currentQno === this.questionList.length) {
       this.isQuizCompleted = true;
       this.stopCounter();
     }
-    if(option.correct){
+    if (option.correct) {
       this.points += 10;
       this.correctAnswer++;
-      setTimeout(()=>{
+      setTimeout(() => {
         this.currentQuestion++;
         this.resetCounter();
       }, 1000);
-    }
-    else{
-      setTimeout(()=>{
+    } else {
+      setTimeout(() => {
         this.incorrectAnswer++;
         this.currentQuestion++;
-        this.resetCounter()
+        this.resetCounter();
       }, 1000);
       this.points -= 10;
     }
   }
 
-  startCounter(){
-    this.interval$ = interval(1000).subscribe(val => {
+  startCounter() {
+    this.interval$ = interval(1000).subscribe((val) => {
       this.counter--;
-      if(this.counter === 0){
-          if(this.currentQuestion+1 >= this.questionList.length){
-            this.isQuizCompleted = true;
-            this.stopCounter();
-          }
-          else{
-            this.currentQuestion++;
-            this.counter = this.timer;
-            this.points -= 10;
-          }
+      if (this.counter === 0) {
+        if (this.currentQuestion + 1 >= this.questionList.length) {
+          this.isQuizCompleted = true;
+          this.stopCounter();
+        }
+
+        this.currentQuestion++;
+        this.counter = this.timer;
+        this.points -= 10;
       }
-      
     });
 
-    setTimeout(()=>{
+    setTimeout(() => {
       this.interval$.unsubscribe();
     }, 600000);
   }
 
-  stopCounter(){
+  stopCounter() {
     this.interval$.unsubscribe();
     this.counter = 0;
   }
 
-  resetCounter(){
+  resetCounter() {
     this.stopCounter();
     this.counter = this.timer;
     this.startCounter();
   }
 
-  resetQuiz(){
+  resetQuiz() {
     this.resetCounter();
     this.getAllQuestions();
     this.points = 0;
